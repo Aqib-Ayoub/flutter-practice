a62af64802777d04c2fe2089b202c0ef3361742f
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-09-01 20:56:46 +0530

    added styled container widget

---
+++ lib/styled_container.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StyledContainer extends StatelessWidget {
+  final String text;
+  const StyledContainer({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
